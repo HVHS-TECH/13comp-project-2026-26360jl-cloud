9cb3f8508bc87963d34a64e44252bed9c3b03fe1
26360jl-cloud <26360jl@hvhs.school.nz>
2026-03-06 14:26:23 +1300

    queue for gtn working

--- gtn.js
+++ gtn.js
@@ -1,14 +1,57 @@
-function test()
+userInQueue = false
+
+firebase.database().ref('/queue/').on('value', readQueue);
+
+function readQueue(snapshot)
 {
-    document.getElementById("profileImage").src = getUserInfo().photoUrl
+    document.getElementById("profile").src = userInfo.photoUrl
+    const queueText = document.getElementById("queueCount")
+    if (snapshot.val() == null)
+    {
+        queueText.innerHTML = "0 players in queue"
+        return
+    }
+
+    const userUids = Object.keys(snapshot.val());
+    const userTimestamps = Object.values(snapshot.val());
+    var queueLength = userTimestamps.length
+
+    queueText.innerHTML = queueLength + " players in queue"
+
+    if (userInQueue)
+        queueText.innerHTML += " (you are in queue)"
+    else
+        return // if the user is not queueing it doesn't care about starting games its not apart of
+
+    if (queueLength < 2)
+        return
+    
+    if (userUids[0] == getUserInfo().uid)
+    {
+        console.log(userTimestamps[0].lobbyId)
+        firebase.database().ref('liveGames/' + userTimestamps[0].lobbyId).set(
+        {
+            lobbyId: "test"
+        }
+    )
+    }
 }
 
 function queue()
 {
-    userInfo = getUserInfo()
-    firebase.database().ref('queue/'+userInfo.uid).set(
+    userInQueue = true
+    const userInfo = getUserInfo()
+    const lobbyId = crypto.randomUUID()
+    firebase.database().ref('queue/' + userInfo.uid).set(
         {
-            timestamp: Date.now()
+            lobbyId: lobbyId
         }
     )
+}
+
+function leaveQueue()
+{
+    userInQueue = false
+    const userInfo = getUserInfo()
+    firebase.database().ref('queue/' + userInfo.uid).remove()
 }
