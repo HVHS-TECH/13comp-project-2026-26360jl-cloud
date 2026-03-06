83eaaf6d4eac1dcfe1d1ca8ae72fd28573ff2695
26360jl-cloud <26360jl@hvhs.school.nz>
2026-03-06 15:13:22 +1300

    spell 5 friday

--- gtn.js
+++ gtn.js
@@ -1,10 +1,12 @@
+const maxTimestepDifference = 10000 //If another users timestep has a 10 second difference then they get kicked from queue
+
 userInQueue = false
 
 firebase.database().ref('/queue/').on('value', readQueue);
 
 function readQueue(snapshot)
 {
-    document.getElementById("profile").src = userInfo.photoUrl
+    /*document.getElementById("profile").src = userInfo.photoUrl
     const queueText = document.getElementById("queueCount")
     if (snapshot.val() == null)
     {
@@ -30,11 +32,13 @@ function readQueue(snapshot)
     {
         console.log(userTimestamps[0].lobbyId)
         firebase.database().ref('liveGames/' + userTimestamps[0].lobbyId).set(
-        {
-            lobbyId: "test"
-        }
-    )
-    }
+            {
+                lobbyId: "test",
+                user1: userUids[0],
+                user2: userUids[1]
+            }
+        )
+    }*/
 }
 
 function queue()
@@ -42,11 +46,44 @@ function queue()
     userInQueue = true
     const userInfo = getUserInfo()
     const lobbyId = crypto.randomUUID()
-    firebase.database().ref('queue/' + userInfo.uid).set(
+    /*firebase.database().ref('queue/' + userInfo.uid).set(
         {
-            lobbyId: lobbyId
+            lobbyId: lobbyId,
+            timestamp: Date.now()
         }
-    )
+    )*/
+
+    var ref = firebase.database().ref("queue/" + userInfo.uid);
+    ref.update({
+        onlineState: Date.now(),
+        status: "I'm online."
+    });
+    ref.onDisconnect().update({
+        onlineState: false,
+        status: "I'm offline."
+    });
+
+    //pingQueue(userInfo, lobbyId)
+}
+
+//this function is to ping the server with a timestamp every 3 seconds.
+//the other clients then check that everyones timestamp is up to date.
+//when the differences in timestamps are too big, it probably means the user has closed the tab so they are no longer wanting to queue
+//when this happens the other users will kick them out of the queue
+async function pingQueue(userInfo, lobbyId) {
+    var ref = firebase.database().ref('queue/' + userInfo.uid + "/timestamp").set(Date.now())
+    ref.onDisconnect().set(false);
+
+    await delay(3000);
+
+    console.log("Waited 3 seconds!");
+
+    if (userInQueue)
+        pingQueue(userInfo, lobbyId)
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 }
 
 function leaveQueue()
