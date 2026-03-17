40f60f8683dc916a845c23d789daede368285ed5
26360jl-cloud <26360jl@hvhs.school.nz>
2026-03-18 11:58:17 +1300

    fixed signup stuff

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-    firebase.auth().onAuthStateChanged((user) => {
+document.addEventListener("DOMContentLoaded", async function () {
+    firebase.auth().onAuthStateChanged(async (user) => {
+        console.log(user)
+        if (user == null)
+            return
+
+        if (! await checkFirebase(user))
+            return
+
         if (user)
         {
             window.location.replace("./games.html")
@@ -12,23 +19,25 @@ function login()
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         var userInfo = result.user;
-
-        checkFirebase(userInfo)
     })
 }
 
-function checkFirebase(userInfo)
+async function checkFirebase(userInfo)
 {
-    firebase.database().ref('/registeredUsers/' + userInfo.uid).once('value', _checkFirebase);
+    var userInFirebase = false
+    await firebase.database().ref('/registeredUsers/' + userInfo.uid).once('value', _checkFirebase);
 
-    function _checkFirebase(snapshot)
+    async function _checkFirebase(snapshot)
     {
+        console.log(snapshot.val())
         //if user is not registered in firebase
         if (snapshot.val() == null)
         {
+            userInFirebase = false
             alert("you are not signed up")
-            window.location.replace("./signup.html")
-            return
-        }
+            await window.location.replace("./signup.html")
+        } else userInFirebase = true
     }
+
+    return userInFirebase
 }
