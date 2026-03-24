6f44f3e2788ad9e8e98a918774b802f7731dac09
26360jl-cloud <26360jl@hvhs.school.nz>
2026-03-25 12:31:14 +1300

    working on the leaderboard stuff

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,12 +1,60 @@
+var leaderboardData__;
+var tempNameRenameLater = new Map();
+var orderedData = []
+
 document.addEventListener("DOMContentLoaded", function () {
-    //firebase.database().ref('/leaderboard/').once('value', loadLeaderboard);
     firebaseRead("/leaderboard/", function(snapshot){
-        console.log(snapshot.val())
+        if (snapshot.val() == null) return
+
+        leaderboardData = snapshot.val();
+        leaderboardData__ = leaderboardData;
+        showLeaderboardList(leaderboardData)
     })
 })
 
-function loadLeaderboard(snapshot)
+function bubbleSort()
 {
-    console.log(snapshot.val())
-    
+
+}
+
+function sortByGTNWins()
+{
+    const USER_UIDS = Object.keys(leaderboardData__);
+    const USER_STATS = Object.values(leaderboardData__);
+    var unordered = []
+
+    for (var i = 0; i < USER_UIDS.length; i++)
+    {
+        tempNameRenameLater.set(USER_UIDS[i], USER_STATS[i].gtnWins);
+        unordered[i] = USER_STATS[i].gtnWins;
+    }
+
+    console.log(unordered)
+}
+
+function sortByGTNLosses()
+{
+
+}
+
+function sortByHighscore()
+{
+
+}
+
+async function showLeaderboardList(leaderboardData)
+{
+    sortByGTNWins()
+    const ulElement = document.getElementById("leaderboardList");
+
+    const USER_UIDS = Object.keys(leaderboardData);
+    const USER_STATS = Object.values(leaderboardData);
+
+    for (var i = 0; i < USER_UIDS.length; i++)
+    {
+        const liElement = document.createElement("li");
+        const userInfo = await getUserInfoFromUID(USER_UIDS[i])
+        liElement.textContent = userInfo.name
+        ulElement.appendChild(liElement)
+    }
 }
