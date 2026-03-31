b207edd14ca4ba66d47382198cb9111c0dba8448
26360jl-cloud <26360jl@hvhs.school.nz>
2026-04-01 12:15:26 +1300

    leaderboard stuff

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,8 +1,13 @@
 var leaderboardData__;
-var tempNameRenameLater = new Map();
 var orderedData = []
+var orderedUids = []
 
 document.addEventListener("DOMContentLoaded", function () {
+    refreshLeaderboard();
+})
+
+function refreshLeaderboard()
+{
     firebaseRead("/leaderboard/", function(snapshot){
         if (snapshot.val() == null) return
 
@@ -10,51 +15,100 @@ document.addEventListener("DOMContentLoaded", function () {
         leaderboardData__ = leaderboardData;
         showLeaderboardList(leaderboardData)
     })
-})
-
-function bubbleSort()
-{
-
 }
 
-function sortByGTNWins()
+const TYPE_GTN_WINS = 0;
+const TYPE_GTN_LOSSES = 1;
+const TYPE_HIGHSCORE = 2;
+
+function sortSelectedStat(type)
 {
     const USER_UIDS = Object.keys(leaderboardData__);
     const USER_STATS = Object.values(leaderboardData__);
     var unordered = []
+    var unordered_uids = []
 
-    for (var i = 0; i < USER_UIDS.length; i++)
+    switch (type)
     {
-        tempNameRenameLater.set(USER_UIDS[i], USER_STATS[i].gtnWins);
-        unordered[i] = USER_STATS[i].gtnWins;
+        case TYPE_GTN_WINS:
+            for (var i = 0; i < USER_UIDS.length; i++)
+            {
+                unordered[i] = USER_STATS[i].gtnWins;
+                unordered_uids[i] = USER_UIDS[i];
+            }
+            break;
+        case TYPE_GTN_LOSSES:
+            for (var i = 0; i < USER_UIDS.length; i++)
+            {
+                unordered[i] = USER_STATS[i].gtnLosses;
+                unordered_uids[i] = USER_UIDS[i];
+            }
+            break;
+        case TYPE_HIGHSCORE:
+            for (var i = 0; i < USER_UIDS.length; i++)
+            {
+                unordered[i] = USER_STATS[i].highscore;
+                unordered_uids[i] = USER_UIDS[i];
+            }
+            break
     }
 
     console.log(unordered)
+    console.log(unordered_uids)
+
+    sort(unordered, USER_UIDS);
 }
 
-function sortByGTNLosses()
+function sort(unordered, unordered_uids)
 {
+    for (var i = 0; i < unordered.length; i++)
+    {
+        for (var j = 0; j < unordered.length; j++)
+        {
+            if (j == unordered.length) continue
 
-}
+            if (unordered[j] < unordered[j + 1])
+            {
+                temp = unordered[j]
+                unordered[j] = unordered[j + 1]
+                unordered[j + 1] = temp
 
-function sortByHighscore()
-{
-
+                tempUid = unordered_uids[j]
+                unordered_uids[j] = unordered_uids[j + 1]
+                unordered_uids[j + 1] = tempUid
+            }
+        }
+    }
+    orderedData = unordered
+    orderedUids = unordered_uids
 }
 
 async function showLeaderboardList(leaderboardData)
 {
-    sortByGTNWins()
+    await sortSelectedStat(getTypeFromOption())
     const ulElement = document.getElementById("leaderboardList");
 
-    const USER_UIDS = Object.keys(leaderboardData);
-    const USER_STATS = Object.values(leaderboardData);
+    while (ulElement.children.length > 0)
+    {
+        ulElement.children[0].remove();
+    }
 
-    for (var i = 0; i < USER_UIDS.length; i++)
+    for (var i = 0; i < orderedUids.length; i++)
     {
         const liElement = document.createElement("li");
-        const userInfo = await getUserInfoFromUID(USER_UIDS[i])
-        liElement.textContent = userInfo.name
+        const userInfo = await getUserInfoFromUID(orderedUids[i])
+        liElement.textContent = "#" + (i + 1) + " " + userInfo.name + " : " + orderedData[i]
         ulElement.appendChild(liElement)
     }
+}
+
+function getTypeFromOption()
+{
+    const selectElement = document.getElementById("sortOption").value
+    if (selectElement == "gtnWins")
+            return TYPE_GTN_WINS;
+    if (selectElement == "gtnLosses")
+            return TYPE_GTN_LOSSES;
+    if (selectElement == "highscore")
+            return TYPE_HIGHSCORE;
 }
