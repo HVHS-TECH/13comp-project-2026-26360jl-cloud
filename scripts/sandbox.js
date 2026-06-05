2e601f124cb94ca90fa63513da77099f7cf793c8
26360jl-cloud <26360jl@hvhs.school.nz>
2026-06-05 14:45:45 +1200

    324567890-

--- scripts/sandbox.js
+++ scripts/sandbox.js
@@ -3,7 +3,7 @@ var temp = false
 
 function joinQueue()
 {
-    //if (userInQueue) return
+    if (userInQueue) return
 
     userInQueue = true
     firebaseWrite("queue/" + userInfo.uid, {timestamp: Date.now()})
@@ -15,8 +15,13 @@ function joinQueue()
         //if you are the first person in queue
         if (queueLength == 1)
         {
-            console.log("i am matchmaker")
             firebaseRef("queue").on('value', matchmake)
+        }
+        else if (queueLength == 2)
+        {
+            var sorted = Object.keys(snapshot.val()).sort((a, b) => snapshot.val()[a].timestamp - snapshot.val()[b].timestamp)
+            if (sorted[0] == userInfo.uid)
+                firebaseRef("queue").on('value', matchmake)
         }
     })
 
@@ -33,8 +38,15 @@ function joinQueue()
 
 function matchmake(snapshot)
 {
-firebaseRef("queue").on('value', (snapshot) => {
-        
+    console.log("i am matchmaking")
+    firebaseRef("queue").on('value', (snapshot) => {
+        var queueLength = Object.keys(snapshot.val()).length;
+        var sorted = Object.keys(snapshot.val()).sort((a, b) => snapshot.val()[a].timestamp - snapshot.val()[b].timestamp)
+
+        if (queueLength >= 2)
+        {
+            console.log("matchmake " + sorted[0] + " + " + sorted[1])
+        }
     })
 }
 
